38b241b75b33be25ee511cfd3a826a9a22e60a3e
Rohijit Sinha <sinharohijit97@mgmail.com>
2023-08-01 20:44:04 +0530

    669_trim_a_binary_tree_golang

--- 669_trim_a_binary_search_tree/solution.go
+++ 669_trim_a_binary_search_tree/solution.go
@@ -5,5 +5,21 @@ type TreeNode struct {
 }
 
 func trimBST(root *TreeNode, low int, high int) *TreeNode {
-
+	if root == nil {
+		return nil
+	}
+	if root.Val == low && root.Val == high {
+		root.Left = nil
+		root.Right = nil
+		return root
+	}
+	if root.Val < low {
+		return trimBST(root.Right, low, high)
+	} else if root.Val > high {
+		return trimBST(root.Left, low, high)
+	} else {
+		root.Left = trimBST(root.Left, low, high)
+		root.Right = trimBST(root.Right, low, high)
+		return root
+	}
 }
